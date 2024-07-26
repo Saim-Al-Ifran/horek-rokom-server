ba3d2e05e495baee5423e8cd2c53b4f724510ce7
Saim <admin@gmail.com>
2024-07-26 19:37:26 +0600

    added custom error class

---
+++ src/errors/customError.ts
@@ -0,0 +1,12 @@
+class CustomError extends Error {
+    status: number;
+  
+    constructor(message: string, status: number) {
+      super(message);
+      this.name = this.constructor.name;
+      this.status = status;
+    }
+  }
+  
+  export default CustomError;
+  
